ffcfcbcf15c8636d47ea10db612a537e76fd96b2
Ryan Fickenscher <rsficken@gmail.com>
2022-10-09 02:46:39 -0700

    chapter 2, section 2.5, shader source files

---
+++ chapter2/2.5.shader_source_files/src/glsl_shaders/simple_vs.glsl
@@ -0,0 +1,6 @@
+attribute vec3 aVertexPosition; // Vertex shader expects one position
+void main(void) {
+  // Convert the `vec3` into a `vec4` for scan conversion and
+  // assign to `gl_Position` to pass the vertex to the fragment shader
+  gl_Position = vec4(aVertexPosition, 1.0);
+}
---
+++ chapter2/2.6.parameterized_fragment_shader/src/glsl_shaders/white_fs.glsl
@@ -0,0 +1,6 @@
+precision mediump float; // precision for float computation
+void main(void) {
+  // For every pixel called (within the square) sets
+  // constant color white with alpha-channel value of 1.0
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
